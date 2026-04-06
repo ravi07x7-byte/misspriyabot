2d82e16611f828c9dceb338c10e0287bf420d114
ravi07x7-byte <ravi07x7@gmail.com>
2026-04-07 03:29:45 +0530

    Update and rename ee.py to .env

--- ee.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-
-# Different formats
-print(f"Hello {name}, you are {age} years old.")      # F-string
-print("Hello {}, you are {} years old.".format(name, age)) # .format()
-print("Hello %s, you are %d years old." % (name, age))    # % operator
